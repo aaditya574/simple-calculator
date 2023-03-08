b82778da12d3306eb9b7d693ead6a273fc80aed1
addission <108756447+aad999@users.noreply.github.com>
2023-03-09 04:06:13 +0530

    Initial commit

--- script.js
+++ script.js
@@ -7,34 +7,33 @@ Array.from(buttons).forEach((button)=>{
     )
 }
 )
-document.addEventListener('onkeypress', (e)=>{
-    var key = e.key;
-    process(key);
-})
 function process(key){
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const symbols = ['(', ')', '+', '-', '*', '/', '.'];
     const undefs = ['Infinity', '-Infinity', 'undefined', 'NaN'];
     if(key == 'CL'){
         let string = "";
-        document.querySelector('.screen').innerHTML = string;
+        document.querySelector('.screen').value = string;
     }
     else if(key == 'BK'){
-        let string = document.querySelector('.screen').innerHTML;
+        let string = document.querySelector('.screen').value;
         if(undefs.includes(string)) string  = "";
         else string = string.slice(0, -1);
-        document.querySelector('.screen').innerHTML = string;
+        document.querySelector('.screen').value = string;
     }
     else if(key == 'EV'){
-        let string = document.querySelector('.screen').innerHTML;
+        let string = document.querySelector('.screen').value;
         if(undefs.includes(string)) string  = "";
-        else string = eval(string);
-        document.querySelector('.screen').innerHTML = string;
+        else if(string) string = eval(string);
+        document.querySelector('.screen').value = string;
     }
     else{
-        let string = document.querySelector('.screen').innerHTML;
+        let string = document.querySelector('.screen').value;
         if(undefs.includes(string)) string  = "";
         if(numbers.includes(key) || symbols.includes(key))string = string + key;
-        document.querySelector('.screen').innerHTML = string;
+        document.querySelector('.screen').value = string;
     }
+}
+function update(event){
+    if(event.key == "Enter") process('EV');
 }
